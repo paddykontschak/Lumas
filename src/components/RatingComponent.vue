e82a5d222fc8ce033b5fcce18aef3df070da4637
Patrick Kontschak <patrick.kontschak@mymuesli.com>
2025-06-04 14:12:19 +0200

    Create a component to show 3 star ratings
    It'll show the 3 required star ratings.
    In production scores would come from the database
    checking the existing scores the user has given
    the item in each category

---
+++ src/components/RatingComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+import StarRating from './StarRating.vue'
+</script>
+
+<template>
+    <StarRating category="Fits to my Wall" score=0 />
+    <StarRating category="That's my Style" score=0 />
+    <StarRating category="Enjoy this Artist" score=0 />
+</template>
+
+<style scoped>
+</style>
