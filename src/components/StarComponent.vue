ef5696df977d75d71ed9ddd700cfff0796ae2620
Patrick Kontschak <patrick.kontschak@mymuesli.com>
2025-06-04 14:03:43 +0200

    Create a reusable Star component

---
+++ src/components/StarComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps([
+  'state'
+])
+</script>
+
+<template>
+  <span class="star" v-if="state == 'full'">★</span>
+  <span class="star" v-else-if="state == 'empty'">☆</span>
+</template>
+
+<style scoped>
+.star {
+  font-size: 24px;
+  vertical-align: middle;
+  cursor: pointer;
+}
+</style>
