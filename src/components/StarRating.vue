54c95697e65e0343445df2b11cde395cf216d21d
Patrick Kontschak <patrick.kontschak@mymuesli.com>
2025-06-04 14:10:23 +0200

    Create a component that shows 5 stars
    It'll also display the name of the category
    and the score

---
+++ src/components/StarRating.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Star from './StarComponent.vue'
+defineProps({
+  category: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="star-rating">
+    <span class="category">{{category}}</span>
+    <Star
+      v-for="n in 5"
+      :key="n"
+    />
+    <span class="score">{{score}} / 5</span>
+  </div>
+</template>
+
+<style scoped>
+.category {
+  display: block;
+}
+
+.score {
+  padding: 0 15px;
+}
+</style>
